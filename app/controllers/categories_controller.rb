16bc09fd0b574709c6ecd4d1ffced44ba33bb4d4
Valeria Copleman <valeriacopleman@gmail.com>
2020-12-07 06:35:08 -0800

    fix restaurant controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,16 +3,8 @@ class CategoriesController < ApplicationController
     before_action :authenticate_user!
     
     def index
-        @categories = current_user.categories.all
+        @categories = current_user.categories.uniq
         
-    end
-
-    def show
-        @category = current_user.categories.find_by(id: params[:id])
-        @restaurant = current_user.restaurants
-        if !@category
-            redirect_to root_path
-        end
     end
     
     def new 
@@ -21,19 +13,12 @@ class CategoriesController < ApplicationController
     end
 
     def create 
-        category = Category.find_by(name: category_params[:name])
+        category = Category.find_by(name: params[:name])
         if !category
             category = current_user.categories.create(category_params)
         end
         redirect_to category
        
-    end
-
-    def destroy
-        @restaurants = Restaurant.where(category_id: params[:category.id])
-        @restaurants.each do |restaurant|
-            restaurant.destroy
-        end
     end
 
     private
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,8 +3,9 @@ class RestaurantsController < ApplicationController
     before_action :authenticate_user!
 
     def index
-        if params[:category_id] && @category = Category.find_by(id: params[:category_id])
-            @restaurants = current_user.restaurants
+        if params[:category_id] && @category = current_user.categories.find_by(id: params[:category_id])
+            #binding.pry
+            @restaurants = current_user.restaurants.where(category_id: @category.id)
         else 
             redirect_to root_path
         end
@@ -19,15 +20,15 @@ class RestaurantsController < ApplicationController
     end
 
     def new
-        if params[:category_id] && @category = Category.find_by(id: params[:category_id])
+        if params[:category_id] && @category = current_user.categories.find_by(id: params[:category_id])
             @restaurant = @category.restaurants.build
         else
-            @restaurant = Restaurant.new
+            @restaurant = current_user.restaurants.build
         end
     end
 
     def create
-        if params[:category_id] && @category = Category.find_by(id: params[:category_id])
+        if params[:category_id] && @category = current_user.categories.find_by(id: params[:category_id])
             @restaurant = current_user.restaurants.build(restaurant_params)
             if @restaurant.save
                 redirect_to category_restaurants_path
